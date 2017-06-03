1d5e2bcc7fb73658be5596d8104b97f7238c5350
Rockyzsu <jinweizsu@gmail.com>
2017-06-03 14:31:49 +0800

    first edition

---
+++ iqiyi_tsos.py
@@ -0,0 +1,25 @@
+# -*-coding=utf-8-*-
+__author__ = 'Rocky'
+'''
+http://30daydo.com
+Contact: weigesysu@qq.com
+'''
+from uiautomator import device as d
+import time
+class IQIYI_Stress():
+
+    def __init__(self):
+        pass
+
+
+    def getLog(self):
+        pass
+
+    def testcase(self):
+        d.press.down()
+        time.sleep(2)
+
+
+
+obj=IQIYI_Stress()
+obj.testcase()
